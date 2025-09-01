6a6d88c6350de4f179bd80090994896c9e702881
DB Miklofsky <dmiklofsky@wccnet.edu>
2025-08-31 20:47:22 -0400

    updates

---
+++ Assignments/Starting Project 1/settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CPS.251.B01_DB_Miklofsky_Starter_Project_1"
+include(":app")
+include(":app")
+ 
